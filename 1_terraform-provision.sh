ddd4d3f0f045085879897fe27d99598e7236f4c2
Rudy Gevaert <Rudy.Gevaert@gig.tech>
2019-04-05 13:17:21 +0200

    Add info about connecting to mgt host

--- 1_terraform-provision.sh
+++ 1_terraform-provision.sh
@@ -4,5 +4,7 @@ cd terraform
 . iyo.env
 terraform init
 terraform plan
-terraform apply
+terraform apply -auto-approve
 cd ..
+
+echo "Login to the above machine's IP address with user ansible and port 2222"
