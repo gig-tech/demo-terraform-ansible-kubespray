e1e39e57cb367d59ab8eb219f27dc5f3bf7e56da
Rudy Gevaert <Rudy.Gevaert@gig.tech>
2019-04-05 12:55:23 +0200

    Fix shebang

--- 2_ansible.sh
+++ 2_ansible.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#!/bin/sh
 
 ANSIBLE_TF_DIR=terraform/ python provisioning/inventory/terraform.py > provisioning/inventory/demo.json
 ansible-playbook -i provisioning/inventory/demo  provisioning/provision-k8s-nodes.yml
